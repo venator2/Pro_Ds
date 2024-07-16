a6a5bedffba1880fda644f9b6d3e3423aa20fefb
venator2 <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>
2024-07-16 14:55:24 +0300

    Use python3 -m pip to ensure pip is found and install dependencies

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,11 @@
 # Ensure pip is installed
 python3 -m ensurepip --upgrade
 
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
 # Install dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput
