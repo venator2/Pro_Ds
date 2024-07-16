2557ed612e622534494b1f8a8cbf7c69a927ac3f
venator2 <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>
2024-07-16 14:43:01 +0300

    Make build_files.sh executable

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Ensure pip is installed
+python3 -m ensurepip --upgrade
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput
