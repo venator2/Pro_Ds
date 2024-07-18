6444690fcc466270bf0d4633d8fd7de6da9b40d5
venator2 <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>
2024-07-18 11:02:28 +0300

    Use Postgresql

--- build_files.sh
+++ build_files.sh
@@ -11,3 +11,6 @@ python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput
+
+# Run migrations
+python3 manage.py migrate
--- my_store/settings.py
+++ my_store/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'venator2',
+        'USER': 'venator2',
+        'PASSWORD': 'BOGDAN200konon',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
