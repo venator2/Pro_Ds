6126e8b24efa5d945fc793a71bc7d585d3495fd6
venator2 <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>
2024-07-21 18:24:56 +0300

    python  deploy

--- my_store/settings.py
+++ my_store/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'venator2',
-        'USER': 'venator2',
-        'PASSWORD': 'BOGDAN200konon',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] = dj_database_url.config()
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'venator2',
+#        'USER': 'venator2',
+#        'PASSWORD': 'BOGDAN200konon',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
+#DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
