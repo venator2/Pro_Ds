746f6886c9e939d07b60f19948fa9e3919b3f21f
venator2 <bohdan.konon@gmail.com>
2024-08-04 16:23:15 +0300

    deploy 5

--- my_store/wsgi.py
+++ my_store/wsgi.py
@@ -8,12 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+
+# додайте шлях до вашого проекту
+path = '/home/venator3/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
+# встановіть змінну оточення для вашого проекту
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_store.settings'
+
+# завантажте Django WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_store.settings')
-
-
 application = get_wsgi_application()
-# add this vercel variable
-app = application
