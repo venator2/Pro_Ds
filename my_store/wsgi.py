4c7792eb21821b63c12770578b9bde04ec35ceae
venator2 <bohdan.konon@gmail.com>
2024-08-05 22:47:05 +0300

    deploy 5

--- my_store/wsgi.py
+++ my_store/wsgi.py
@@ -22,22 +22,3 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'my_store.settings'
 # завантажте Django WSGI
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-import os
-import sys
-
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-## Uncomment the lines below depending on your Django version
-###### then, for Django >=1.5:
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-###### or, for older Django <=1.4
-#import django.core.handlers.wsgi
-#application = django.core.handlers.wsgi.WSGIHandler()
