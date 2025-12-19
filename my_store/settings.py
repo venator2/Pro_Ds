500a31abbc09149c2c992e15ad524085410480d0
venator2 <bohdan.konon@gmail.com>
2025-12-19 23:20:46 +0200

    Prepare for Render deploy

--- my_store/settings.py
+++ my_store/settings.py
@@ -83,13 +83,7 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -124,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Шлях до зібраних (зібраних) статичних файлів.
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
